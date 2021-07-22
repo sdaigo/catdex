0d26c6a4e1a23712d0d354db5914b5b47655efe9
Daigo Shitara <stain.witness+github@gmail.com>
2021-07-22 16:23:13 +0900

    static site

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::Files;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use handlebars::Handlebars;
+use serde_json::json;
+
+// request handler
+async fn index(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+    let data = json!({
+        "project_name": "Catdex",
+        "cats": [
+            {
+                "name": "British short hair",
+                "image_path": "/static/image/british-short-hair.jpg"
+            },
+            {
+                "name": "Persian",
+                "image_path": "/static/image/persian.jpg"
+            },
+            {
+                "name": "Ragdoll",
+                "image_path": "/static/image/ragdoll.jpg"
+            }
+        ]
+    });
+
+    let body = hb.render("index", &data).unwrap();
+
+    HttpResponse::Ok().body(body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./static/")
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+
+    println!("Listening on port 8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(handlebars_ref.clone())
+            .service(Files::new("/static", "static").show_files_listing())
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
